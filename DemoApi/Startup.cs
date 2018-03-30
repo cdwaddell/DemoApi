796aa3aa2d35acf013491f9ffc88cb124470ae62
CD Waddell <daniel.waddell@scholarrx.com>
2018-03-30 09:45:19 -0400

    route

--- DemoApi/Startup.cs
+++ DemoApi/Startup.cs
@@ -265,6 +265,7 @@ namespace DemoApi
 
                 app.UseSwaggerUI(options =>
                 {
+                    options.RoutePrefix = "swagger/ui";
                     foreach (var version in ApiConstants.Versions)
                     {
                         options.SwaggerEndpoint($"/swagger/v{version}/swagger.json", version);
